387000f4d49c6f4d540d24b6aa3ac31a32a6e33e
Man, Bliss <bliss.man@bellmedia.ca>
2020-02-24 14:58:56 -0500

    update packages, update jest arguments

--- gulpfile.js
+++ gulpfile.js
@@ -179,11 +179,7 @@ gulp.task('babel', () =>
     karma tasks
 */
 gulp.task('test', function() {
-    return jest.runCLI({
-        config: {
-            rootDir: 'test/'
-        }
-    }, '.');
+    return jest.runCLI({}, ['test/']);
 });
 
 /*
