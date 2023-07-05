a48b468630aa66247ed8254f936835bcc4cc64fd
Bliss Man <bliss.man@bellmedia.ca>
2023-07-05 17:27:20 -0400

    update eslint version to accommodate new ECMAScript standards

--- gulpfile.js
+++ gulpfile.js
@@ -5,46 +5,48 @@ const beautify = require('gulp-jsbeautifier');
 // clean
 const del = require('del');
 // eslint
-const eslint = require('gulp-eslint');
+const eslint = require('gulp-eslint-new');
 const eslintConfig = {
-    "env": {
-        "browser": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 6
-    },
-    // See https://eslint.org/docs/rules/ for rules
-    "rules": {
-        "indent": [
-            "error",
-            4,
-            {
-                "SwitchCase": 1
-            }
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-console": 0, // allow console.log etc
-        "no-extra-boolean-cast": 0, // allow using !! to cast to boolean
-        "no-unused-vars": ["error",
-            {
-                "vars": "all", // no unused variables in any scope
-                // allow trailing unused args since functions may be called with
-                // them even if they aren't used in the function
-                "args": "none"
-            }
-        ],
-        "no-trailing-spaces": ["error"],
-        "curly": ["error"],
-        "no-extend-native": ["error"],
-        "prefer-const": ["error"]
+    "overrideConfig": {
+        "env": {
+            "browser": true,
+            "amd": true,
+            "node": true,
+            "es2023": true
+        },
+        "extends": "eslint:recommended",
+        // See https://eslint.org/docs/rules/ for rules
+        "rules": {
+            "indent": [
+                "error",
+                4,
+                {
+                    "SwitchCase": 1
+                }
+            ],
+            "linebreak-style": [
+                "error",
+                "unix"
+            ],
+            "semi": [
+                "error",
+                "always"
+            ],
+            "no-console": 0, // allow console.log etc
+            "no-extra-boolean-cast": 0, // allow using !! to cast to boolean
+            "no-unused-vars": ["error",
+                {
+                    "vars": "all", // no unused variables in any scope
+                    // allow trailing unused args since functions may be called with
+                    // them even if they aren't used in the function
+                    "args": "none"
+                }
+            ],
+            "no-trailing-spaces": ["error"],
+            "curly": ["error"],
+            "no-extend-native": ["error"],
+            "prefer-const": ["error"]
+        }
     }
 };
 // babel
